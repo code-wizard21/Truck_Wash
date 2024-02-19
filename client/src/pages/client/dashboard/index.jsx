52fd2ecdcc293f3a8fc3616d7866af79ca3b1c9a
code-wizard21 <strikepunchkalpha@gmail.com>
2024-02-19 16:31:59 -0500

    #1_client_basic

--- client/src/pages/client/dashboard/index.jsx
+++ client/src/pages/client/dashboard/index.jsx
@@ -1,111 +1,278 @@
-import * as React from "react";
-import { styled } from "@mui/material/styles";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell, { tableCellClasses } from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import Grid from "@mui/material/Grid";
-import { withGoogleMap, withScriptjs, GoogleMap } from "react-google-maps";
-import Container from "@mui/material/Container";
-import CssBaseline from "@mui/material/CssBaseline";
-
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-  [`&.${tableCellClasses.head}`]: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white,
-  },
-  [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
-  },
-}));
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  "&:nth-of-type(odd)": {
-    backgroundColor: theme.palette.action.hover,
-  },
-  // hide last border
-  "&:last-child td, &:last-child th": {
-    border: 0,
-  },
-}));
-
-function createData(name, calories, fat, carbs, protein, state) {
-  return { name, calories, fat, carbs, protein, state };
-}
-
-const rows = [createData("Aiden", "10:30", "Name", "xxxx", "xxx", "complete")];
-
-export default function CustomizedTables() {
+import React, { useState, useEffect } from "react";
+import "./style.css";
+import {
+  Box,
+  Container,
+  Stack,
+  
+  Unstable_Grid2 as Grid,
+} from "@mui/material";
+import axios from "axios";
+import MUIDataTable from "mui-datatables";
+import { CircularProgress, Typography } from "@material-ui/core";
+import { confirmAlert } from "react-confirm-alert";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+const api = axios.create({
+  baseURL: `http://localhost:5000/api`,
+});
+const theme = createMuiTheme();
+const App = () => {
+  const [state, setState] = useState({
+    page: 1,
+    count: 1,
+    rowsPerpage: 10,
+    sortOrder: {},
+    isLoading: false,
+    data: [],
+    editData: [],
+    equipment: [],
+  });
+  useEffect(() => {
+    getData();
+  }, []);
+  const getDateSrc = () => {
+    return [
+      {
+        pageNumber: 1,
+        pageSize: 10,
+        firstPage: null,
+        lastPage: null,
+        totalPages: 0,
+        totalRecords: 14,
+        nextPage: null,
+        previousPage: null,
+        data: [
+          {
+            id: 3,
+            name: "Lenovo_Yoga11",
+            location: "Rue Neuve 123, 1000 Bruxelles",
+            description: "Lenovo Yoga S940",
+            picturePath: null,
+            status: "Waiting",
+            dateAdded: "2020-09-01T00:00:00",
+            maintenances: [],
+          },
+          {
+            id: 7,
+            name: "Toshiba_Satellite11",
+            location: "Boulevard Lambermont 1, 1000 Bruxelles",
+            description: "Toshiba Satellite C55-B5300 16-Inch Laptop",
+            picturePath: null,
+            status: "Waiting",
+            dateAdded: "2020-08-19T00:00:00",
+            maintenances: [],
+          },
+          {
+            id: 5,
+            name: "Toshib_Satellite y11",
+            location: "Chaussée d'Ixelles 84/86, 1050 Ixelles",
+            description: "Toshiba Satellite S55-C5274 15.6 Inch",
+            picturePath: null,
+            status: "Accept",
+            dateAdded: "2020-08-05T00:00:00",
+            maintenances: [],
+          },
+          {
+            id: 4,
+            name: "Lenovo_Ideapad11",
+            location: "Boulevard Sylvain Dupuis 433, 1070 Anderlecht",
+            description: "Lenovo Ideapad 330-15ARR",
+            picturePath: null,
+            status: "Accept",
+            dateAdded: "2020-08-02T00:00:00",
+            maintenances: [],
+          },
+          {
+            id: 2,
+            name: "Lenovo_Legion11",
+            location: "Chaussée de Louvain 610, 1030 Schaerbeek",
+            description: "Lenovo Legion Y740 (81UH006YIN)",
+            picturePath: null,
+            status: "Finish",
+            dateAdded: "2020-08-01T00:00:00",
+            maintenances: [],
+          },
+          {
+            id: 6,
+            name: "TOSHIBA_Laptop11",
+            location: "Rue Saint-Lambert 200, 1200 Woluwe-Saint-Lambert",
+            description: "TOSHIBA Laptop Tecra A50-01R01S Intel Core i7",
+            picturePath: null,
+            status: "Finish",
+            dateAdded: "2020-07-01T00:00:00",
+            maintenances: [],
+          },
+          {
+            id: 1,
+            name: "Lenovo_IdeaPad_Slim_311",
+            location: "Berchem-Sainte-Agathe",
+            description: null,
+            picturePath: null,
+            status: "Finish",
+            dateAdded: null,
+            maintenances: [],
+          },
+        ],
+        succeeded: true,
+        errors: null,
+        message: null,
+      },
+   
+    ];
+  };
+  const getData = () => {
+    setState({
+      ...state,
+      data: getDateSrc()[0].data,
+      isLoading: false,
+      page: getDateSrc()[0].pageNumber,
+      count: getDateSrc()[0].totalRecords,
+    });
+  };
+  const columns = [
+    {
+      name: "name",
+      label: "Name",
+      options: {
+        filter: true,
+        customBodyRenderLite: (dataIndex) => {
+          return <span>{data[dataIndex].name}</span>;
+        },
+      },
+    },
+    {
+      name: "carcode",
+      label: "Car Number",
+      options: {
+        filter: true,
+        customBodyRenderLite: (dataIndex) => {
+          return <span>{data[dataIndex].name}</span>;
+        },
+      },
+    },
+    {
+      name: "description",
+      label: "Description",
+      // options: {
+      //   filter: true,
+      //   customBodyRenderLite: (dataIndex) => {
+      //     return <span>{this.state.data[dataIndex].description}</span>;
+      //   },
+      // },
+    },
+    {
+      name: "status",
+      label: "State",
+      options: {
+        filter: true,
+        customBodyRenderLite: (dataIndex) => {
+          const state = data[dataIndex].status;
+          if (state === "Accept") {
+            return (
+              <div className="operating">
+                <span>{state}</span>
+              </div>
+            );
+          }
+           if (state === "Finish") {
+            return (
+              <div className="finish">
+                <span>{state}</span>
+              </div>
+            );
+          } 
+          else {
+            return (
+              <div className="maintenance">
+                <span>{state}</span>
+              </div>
+            );
+          }
+        },
+      },
+    },
+    {
+      name: "pnumber",
+      label: "Phone Number",
+      options: {
+        filter: true,
+        customBodyRenderLite: (dataIndex) => {
+          return <span>{data[dataIndex].location}</span>;
+        },
+      },
+    },
+    {
+      name: "dateAdded",
+      label: "Date",
+      // options: {
+      //   filter: true,
+      //   customBodyRenderLite: (dataIndex) => {
+      //     return <span>{this.state.data[dataIndex].dateAdded}</span>;
+      //   },
+      // },
+    },
+  ];
+  const { data, count, isLoading, rowsPerpage } = state;
+  const options = {
+    filter: true,
+    filterType: "dropdown",
+    responsive: "vertical",
+    download: false,
+    serverSide: true,
+    print: false,
+    count: count,
+    rowsPerpage: rowsPerpage,
+    rowsPerPageOptions: [],
+    selectableRowsHideCheckboxes: false,
+    selectableRows: "none",
+    selectableRowsHeader: false,
+    onColumnSortChange: (changedColumn, direction) =>
+      console.log("changedColumn: ", changedColumn, "direction: ", direction),
+    onChangeRowsPerPage: (numberOfRows) =>
+      console.log("numberOfRows: ", numberOfRows),
+    onChangePage: (currentPage) => console.log("currentPage: ", currentPage),
+    onTableChange: (action, tableState) => {
+      console.log(action, tableState);
+      //   switch (action) {
+      //   case "changePage":
+      //     changePage(tableState.page);  // <-- Change is here
+      //     break;
+      //   case "sort":
+      //     sort(tableState.page);  // <-- Change is here
+      //     break;
+      //   default:
+      //     console.log("action not handled.");
+      // }
+    },
+  };
+  // ... Rest of your code ...
   return (
-    <React.Fragment>
-      <CssBaseline />
-      <div style={{ marginTop: "120px", width: "100%" }} minWidth="sl">
-        <CssBaseline />
-        <Grid
-          container
-          direction="row"
-          justify="center"
-          alignItems="center"
-          justifyContent="center"
-        >
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2539.501242822434!2d-104.58563572314864!3d50.46901168602675!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x531c1fa7080a1679%3A0x27e82440887ddbb!2s625%20McDonald%20St%2C%20Regina%2C%20SK%20S4N%204X1%2C%20Canada!5e0!3m2!1sen!2s!4v1708079587189!5m2!1sen!2s"
-            width="600"
-            height="450"
-            allowFullscreen=""
-            loading="lazy"
-            reFerrerpolicy="no-referrer-when-downgrade"
-          ></iframe>
-        </Grid>
-        <Grid
-          container
-          direction="column"
-          justify="center"
-          alignItems="center"
-          justifyContent="center"
-        >
-          <TableContainer
-            style={{ marginTop: "40px" }}
-            component={Paper}
-            maxWidth="sl"
-          >
-            <Table sx={{ minWidth: 500 }} aria-label="customized table">
-              <TableHead>
-                <TableRow>
-                  <StyledTableCell>CustomerName</StyledTableCell>
-                  <StyledTableCell align="right">Time</StyledTableCell>
-                  <StyledTableCell align="right">Driver</StyledTableCell>
-                  <StyledTableCell align="right">CarNumber</StyledTableCell>
-                  <StyledTableCell align="right">CARLocation</StyledTableCell>
-                  <StyledTableCell align="right">STATE</StyledTableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {rows.map((row) => (
-                  <StyledTableRow key={row.name}>
-                    <StyledTableCell component="th" scope="row">
-                      {row.name}
-                    </StyledTableCell>
-                    <StyledTableCell align="right">
-                      {row.calories}
-                    </StyledTableCell>
-                    <StyledTableCell align="right">{row.fat}</StyledTableCell>
-                    <StyledTableCell align="right">{row.carbs}</StyledTableCell>
-                    <StyledTableCell align="right">
-                      {row.protein}
-                    </StyledTableCell>
-                    <StyledTableCell align="right">{row.state}</StyledTableCell>
-                  </StyledTableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </Grid>
-      </div>
-
- 
-    </React.Fragment>
+    <Box
+      component="main"
+      sx={{
+        flexGrow: 1,
+        py: 8,
+      }}
+    >
+      <Container maxWidth="lg">
+        <MUIDataTable
+          title={
+            <Typography variant="h6">
+              List of Task
+              {isLoading && (
+                <CircularProgress
+                  size={24}
+                  style={{ marginLeft: 15, position: "relative", top: 4 }}
+                />
+              )}
+            </Typography>
+          }
+          data={data}
+          columns={columns}
+          options={options}
+        />
+      </Container>
+    </Box>
   );
-}
+};
+export default App;
